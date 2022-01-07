43a53857433dcbcab4bfe4582492ecf3ad0dc0df
Nur <raynura182@gmail.com>
2022-01-07 00:43:58 -0800

    added more filter

--- script.js
+++ script.js
@@ -5,30 +5,41 @@ const searchInput = document.getElementById("searchInput");
 const searchBtn = document.querySelector(".fa-search");
 const refresh = document.getElementById("refresh");
 const wrapper = document.querySelector(".wrapper");
+const flashSale = document.querySelector(".winterHoliday");
 const result = document.querySelector(".result");
+const product = document.querySelector(".product");
+const idProductClass = document.getElementsByClassName("idProduct");
 const filterContainer = document.querySelector(".filter-container");
 const idFilterBrand = document.querySelector("#filter-brand");
 const price = document.querySelector("#price");
 const displayTypeDiv = document.querySelector("#filter-display-type");
 const storageDiv = document.querySelector('#storageType');
-const idProductClass = document.getElementsByClassName("idProduct");
-const product = document.querySelector(".product");
-const flashSale = document.querySelector(".winterHoliday");
-const year = document.querySelector("#year");
+const yearDiv = document.querySelector("#years");
+const colorsDiv = document.querySelector("#colors");
+const screenSizeDiv = document.querySelector("#screenSize");
+const screenResolutionDiv = document.querySelector("#screenResolution");
 
 
 //  clearing and refresh page
 function refreshPage() {
   wrapper.style.display = "flex";
   filterContainer.style.display = "none";
-  flashSale.style.height = ''
-  flashSale.style.paddingBottom = ''
+  flashSale.style.height = '';
+  flashSale.style.paddingBottom = '';
+  let isAllCheck = false;
+  Array.from(document.querySelectorAll('input[type=checkbox]')).forEach(el => el.checked = isAllCheck);
+  cleanCheckBbox();
+  cleanMainDiv();
+}
+function cleanMainDiv() {
   while (result.hasChildNodes()) {
     result.removeChild(result.firstChild);
   }
   while (product.hasChildNodes()) {
     product.removeChild(product.firstChild);
   }
+}
+function cleanCheckBbox() {
   while (idFilterBrand.hasChildNodes()) {
     idFilterBrand.removeChild(idFilterBrand.firstChild);
   }
@@ -38,18 +49,31 @@ function refreshPage() {
   while (displayTypeDiv.hasChildNodes()) {
     displayTypeDiv.removeChild(displayTypeDiv.firstChild);
   }
-
-  while (year.hasChildNodes()) {
-    year.removeChild(year.firstChild);
+  while (yearDiv.hasChildNodes()) {
+    yearDiv.removeChild(yearDiv.firstChild);
   }
-
-  let isAllCheck = false;
-  Array.from(document.querySelectorAll('input[type=checkbox]')).forEach(el => el.checked = isAllCheck);
+  while (colorsDiv.hasChildNodes()) {
+    colorsDiv.removeChild(colorsDiv.firstChild);
+  }
+  while (screenSizeDiv.hasChildNodes()) {
+    screenSizeDiv.removeChild(screenSizeDiv.firstChild);
+  }
+  while (screenResolutionDiv.hasChildNodes()) {
+    screenResolutionDiv.removeChild(screenResolutionDiv.firstChild);
+  }
+}
+function callAllFilter() {
+  filterBrandFn();
+  storageTypeFn();
+  displayType();
+  searchByYear();
+  colorFn();
+  screenSizeFn();
+  screenResolutionFn();
 }
 function hideInputText() {
   searchInput.value = "";
 }
-
 //  function to show Result of search
 function resultOfSearch(resultArray) {
   // create DIV element
@@ -80,10 +104,14 @@ function resultOfSearch(resultArray) {
 function filterBrandFn() {
   // create an array of Brand Name
   const dataBrand = [];
-  for (let i = 0; i < data.length; i++) {
-    const brandName = data[i].brand;
-    if (!dataBrand.includes(brandName)) {
-      dataBrand.push(data[i].brand);
+  for (let j = 0; j < idProductClass.length; j++) {
+    let idPrClass = idProductClass[j].innerText;
+    for (let i = 0; i < data.length; i++) {
+      const brandName = data[i].brand;
+      let idPr = data[i].idProduct.toString();
+      if (!dataBrand.includes(brandName) && idPrClass === idPr) {
+        dataBrand.push(data[i].brand);
+      }
     }
   }
   // show in the left container array of Brand Name
@@ -96,8 +124,6 @@ function filterBrandFn() {
     idFilterBrand.appendChild(listOfFilter);
   }
 }
-
-
 // search By checkbox Brand
 function searchByBrand(event) {
   const item = event.target;
@@ -106,10 +132,10 @@ function searchByBrand(event) {
   for (let j = 0; j < idProductClass.length; j++) {
     let idPrClass = idProductClass[j].innerText;
     for (let i = 0; i < data.length; i++) {
-      let brand = data[i].brand.toLowerCase();
+      let brand = data[i].brand;
       let idPr = data[i].idProduct.toString();
 
-      if (item.id.toLowerCase() === brand && idPrClass === idPr) {
+      if (item.id === brand && idPrClass === idPr) {
         console.log(data[i].idProduct.toString());
         brandArray.push(data[i]);
       }
@@ -119,24 +145,26 @@ function searchByBrand(event) {
     result.removeChild(result.firstChild);
   }
   resultOfSearch(brandArray);
+  cleanCheckBbox();
+  callAllFilter();
 }
-
 //SearchByYear
 function searchByYear() {
   const dataYear = [];
-  for (let i = 0; i < data.length; i++) {
-    const year = data[i].year;
-    //console.log(typeof year)
-    //console.log(year)
-    if(!dataYear.includes (year)) {
-      dataYear.push(data[i].year);
+  for (let j = 0; j < idProductClass.length; j++) {
+    let idPrClass = idProductClass[j].innerText;
+    for (let i = 0; i < data.length; i++) {
+      const year = data[i].year;
+      let idPr = data[i].idProduct.toString();
+      if (!dataYear.includes(year) && idPrClass === idPr) {
+        dataYear.push(data[i].year);
+      }
     }
   }
-
-   let yearsOrder = dataYear;
-   yearsOrder.sort(function(a, b) {
-     return a - b
-   });
+  let yearsOrder = dataYear;
+  yearsOrder.sort(function (a, b) {
+    return a - b
+  });
   //  console.log(yearsOrder)
   for (let i = 0; i < dataYear.length; i++) {
     const listOfYear = document.createElement("div");
@@ -144,10 +172,9 @@ function searchByYear() {
         <input type="checkbox" id="${dataYear[i]}" >
         <label for="${dataYear[i]}" id="${dataYear[i]}">${dataYear[i]}</label><br>
             `;
-    year.appendChild(listOfYear);
+    yearDiv.appendChild(listOfYear);
   }
 }
-
 function filterByYear(event) {
   const item = event.target;
   // console.log(item);
@@ -160,7 +187,7 @@ function filterByYear(event) {
       let yearData = data[i].year.toString();
 
       if (idPrClass === idPr && yearData === item.id) {
-         console.log(data[i].year);
+        console.log(data[i].year);
         yearArray.push(data[i]);
       }
     }
@@ -169,14 +196,21 @@ function filterByYear(event) {
     result.removeChild(result.firstChild);
   }
   resultOfSearch(yearArray);
+  cleanCheckBbox();
+  callAllFilter();
 }
 //  Nigina ------******
 function storageTypeFn() {
   const storageTypeArray = [];
-  for (let i = 0; i < data.length; i++) {
-    const storageType = data[i].storageType;
-    if (!storageTypeArray.includes(storageType)) {
-      storageTypeArray.push(data[i].storageType);
+  for (let j = 0; j < idProductClass.length; j++) {
+    let idPrClass = idProductClass[j].innerText;
+    for (let i = 0; i < data.length; i++) {
+      const brandName = data[i].brand;
+      let idPr = data[i].idProduct.toString();
+      const storageType = data[i].storageType;
+      if (!storageTypeArray.includes(storageType) && idPrClass === idPr) {
+        storageTypeArray.push(data[i].storageType);
+      }
     }
   }
   // show in the left container array of Brand Name
@@ -189,7 +223,6 @@ function storageTypeFn() {
     storageDiv.appendChild(listOfFilter);
   }
 }
-
 function searchByStorageType(event) {
   const item = event.target;
   console.log(item);
@@ -211,9 +244,32 @@ function searchByStorageType(event) {
     result.removeChild(result.firstChild);
   }
   resultOfSearch(storageN);
+  cleanCheckBbox();
+  callAllFilter();
 }
 //  Gulzat ----********
+function displayType() {
+  const displayTypeArray = [];
+  for (let j = 0; j < idProductClass.length; j++) {
+    let idPrClass = idProductClass[j].innerText;
+    for (let i = 0; i < data.length; i++) {
+      const displayType = data[i].displayType;
+      let idPr = data[i].idProduct.toString();
+      if (!displayTypeArray.includes(displayType) && idPrClass === idPr) {
+        displayTypeArray.push(data[i].displayType);
+      }
+    }
+  }
 
+  for (let i = 0; i < displayTypeArray.length; i++) {
+    const listOfFilter = document.createElement("div");
+    listOfFilter.innerHTML = `
+        <input type="checkbox" id="${displayTypeArray[i]}" >
+        <label for="${displayTypeArray[i]}" id="${displayTypeArray[i]}">${displayTypeArray[i]}</label><br>
+            `;
+    displayTypeDiv.appendChild(listOfFilter);
+  }
+}
 function searchByDisplayType(event) {
   const item = event.target;
   console.log(item);
@@ -235,27 +291,10 @@ function searchByDisplayType(event) {
     result.removeChild(result.firstChild);
   }
   resultOfSearch(displayTypeArray);
-}
-function displayType() {
-  const displayTypeArray = [];
-  for (let j = 0; j < data.length; j++) {
-    const displayType = data[j].displayType;
-    if (!displayTypeArray.includes(displayType)) {
-      displayTypeArray.push(data[j].displayType);
-    }
-  }
-
-  for (let i = 0; i < displayTypeArray.length; i++) {
-    const listOfFilter = document.createElement("div");
-    listOfFilter.innerHTML = `
-        <input type="checkbox" id="${displayTypeArray[i]}" >
-        <label for="${displayTypeArray[i]}" id="${displayTypeArray[i]}">${displayTypeArray[i]}</label><br>
-            `;
-    displayTypeDiv.appendChild(listOfFilter);
-  }
+  cleanCheckBbox();
+  callAllFilter();
 }
 //  !!!! -- ALL price  ARRAY IN LEFT CONTAINER --- !!!!
-
 function searchByPrice(event) {
   const item = event.target;
   let low = 0;
@@ -335,10 +374,150 @@ function searchByPrice(event) {
     result.removeChild(result.firstChild);
   }
   resultOfSearch(priceArray);
-
+  cleanCheckBbox();
+  callAllFilter();
 }
 //  !!!! -- ALL RESOLUTION ARRAY IN LEFT CONTAINER --- !!!!
+function screenResolutionFn() {
+  // create an array of Brand Name
+  const dataArray = [];
+  for (let j = 0; j < idProductClass.length; j++) {
+    let idPrClass = idProductClass[j].innerText;
+    for (let i = 0; i < data.length; i++) {
+      const filterName = data[i].screenResolution;
+      let idPr = data[i].idProduct.toString();
+      if (!dataArray.includes(filterName) && idPrClass === idPr) {
+        dataArray.push(data[i].screenResolution);
+      }
+    }
+  }
+  // show in the left container array of Brand Name
+  for (let i = 0; i < dataArray.length; i++) {
+    const listOfFilter = document.createElement("div");
+    listOfFilter.innerHTML = `
+        <input type="checkbox" id="${dataArray[i]}" >
+        <label for="${dataArray[i]}" id="${dataArray[i]}">${dataArray[i]}</label><br>
+            `;
+    screenResolutionDiv.appendChild(listOfFilter);
+  }
+}
+// search By checkbox Brand
+function searchByScrResolution(event) {
+  const item = event.target;
+  console.log(item);
+  let filteredArray = [];
+  for (let j = 0; j < idProductClass.length; j++) {
+    let idPrClass = idProductClass[j].innerText;
+    for (let i = 0; i < data.length; i++) {
+      let filterName = data[i].screenResolution;
+      let idPr = data[i].idProduct.toString();
 
+      if (item.id === filterName && idPrClass === idPr) {
+        console.log(data[i].idProduct.toString());
+        filteredArray.push(data[i]);
+      }
+    }
+  }
+  while (result.hasChildNodes()) {
+    result.removeChild(result.firstChild);
+  }
+  resultOfSearch(filteredArray);
+  cleanCheckBbox();
+  callAllFilter();
+}
+function colorFn() {
+  // create an array of Brand Name
+  const dataArray = [];
+  for (let j = 0; j < idProductClass.length; j++) {
+    let idPrClass = idProductClass[j].innerText;
+    for (let i = 0; i < data.length; i++) {
+      const filterName = data[i].color;
+      let idPr = data[i].idProduct.toString();
+      if (!dataArray.includes(filterName) && idPrClass === idPr) {
+        dataArray.push(data[i].color);
+      }
+    }
+  }
+  // show in the left container array of Brand Name
+  for (let i = 0; i < dataArray.length; i++) {
+    const listOfFilter = document.createElement("div");
+    listOfFilter.innerHTML = `
+        <input type="checkbox" id="${dataArray[i]}" >
+        <label for="${dataArray[i]}" id="${dataArray[i]}">${dataArray[i]}</label><br>
+            `;
+    colorsDiv.appendChild(listOfFilter);
+  }
+}
+// search By checkbox Brand
+function searchByColor(event) {
+  const item = event.target;
+  console.log(item);
+  let filteredArray = [];
+  for (let j = 0; j < idProductClass.length; j++) {
+    let idPrClass = idProductClass[j].innerText;
+    for (let i = 0; i < data.length; i++) {
+      let filterName = data[i].color;
+      let idPr = data[i].idProduct.toString();
+
+      if (item.id === filterName && idPrClass === idPr) {
+        console.log(data[i].idProduct.toString());
+        filteredArray.push(data[i]);
+      }
+    }
+  }
+  while (result.hasChildNodes()) {
+    result.removeChild(result.firstChild);
+  }
+  resultOfSearch(filteredArray);
+  cleanCheckBbox();
+  callAllFilter();
+}
+function screenSizeFn() {
+  // create an array of Brand Name
+  const dataArray = [];
+  for (let j = 0; j < idProductClass.length; j++) {
+    let idPrClass = idProductClass[j].innerText;
+    for (let i = 0; i < data.length; i++) {
+      const filterName = data[i].screenSize;
+      let idPr = data[i].idProduct.toString();
+      if (!dataArray.includes(filterName) && idPrClass === idPr) {
+        dataArray.push(data[i].screenSize);
+      }
+    }
+  }
+  // show in the left container array of Brand Name
+  for (let i = 0; i < dataArray.length; i++) {
+    const listOfFilter = document.createElement("div");
+    listOfFilter.innerHTML = `
+        <input type="checkbox" id="${dataArray[i]}" >
+        <label for="${dataArray[i]}" id="${dataArray[i]}">${dataArray[i]}</label><br>
+            `;
+    screenSizeDiv.appendChild(listOfFilter);
+  }
+}
+// search By checkbox 
+function searchByScrSize(event) {
+  const item = event.target;
+  console.log(typeof item.id);
+  let filteredArray = [];
+  for (let j = 0; j < idProductClass.length; j++) {
+    let idPrClass = idProductClass[j].innerText;
+    for (let i = 0; i < data.length; i++) {
+      let filterName = data[i].screenSize.toString();
+      let idPr = data[i].idProduct.toString();
+      if (item.id === filterName && idPrClass === idPr) {
+        console.log(data[i].idProduct.toString());
+        filteredArray.push(data[i]);
+      }
+    }
+  }
+  while (result.hasChildNodes()) {
+    result.removeChild(result.firstChild);
+  }
+  resultOfSearch(filteredArray);
+  cleanCheckBbox();
+  callAllFilter();
+} 1
 // click and show By Category MAIN !!!
 const showByCategory = (event) => {
   const item = event.target;
@@ -354,10 +533,7 @@ const showByCategory = (event) => {
     }
   }
   resultOfSearch(categoryArray);
-  filterBrandFn();
-  storageTypeFn();
-  displayType();
-  searchByYear();
+  callAllFilter();
 };
 
 //  function to show searched Product
@@ -366,9 +542,19 @@ function searchProduct() {
   wrapper.style.display = "none";
   result.style.display = "grid";
   filterContainer.style.display = "block";
-  flashSale.style.height = '200px'
-  flashSale.style.paddingBottom = '20px'
+  flashSale.style.height = '200px';
+  flashSale.style.paddingBottom = '20px';
 
+  // search data
+  const searchData = data.filter((el) => {
+    let valArr = Object.values(el);
+    return valArr.toString().toLowerCase().includes(searchValue);
+  });
+  cleanCheckBbox();
+  cleanMainDiv();
+  resultOfSearch(searchData);
+  setTimeout(hideInputText, 3000);
+  callAllFilter();
   // const newData = data.map(el => {
   //     const newEl = {};
   //     for (const key in el) {
@@ -376,34 +562,6 @@ function searchProduct() {
   //     }
   //     return newEl;
   // });
-
-  // search data
-  const searchData = data.filter((el) => {
-    let valArr = Object.values(el);
-    return valArr.toString().toLowerCase().includes(searchValue);
-  });
-  while (result.hasChildNodes()) {
-    result.removeChild(result.firstChild);
-  }
-  resultOfSearch(searchData);
-  if (searchInput.value === "") {
-    refreshPage();
-  }
-  while (idFilterBrand.hasChildNodes()) {
-    idFilterBrand.removeChild(idFilterBrand.firstChild);
-  }
-  while (displayTypeDiv.hasChildNodes()) {
-    displayTypeDiv.removeChild(displayTypeDiv.firstChild);
-  }
-
-  while (storageDiv.hasChildNodes()) {
-    storageDiv.removeChild(storageDiv.firstChild);
-  }
-  filterBrandFn();
-  storageTypeFn();
-  displayType();
-  setTimeout(hideInputText, 3000);
-  searchByYear();
 }
 //  ---- show one product ------
 function showProduct(id) {
@@ -413,15 +571,8 @@ function showProduct(id) {
   filterContainer.style.display = "none";
   flashSale.style.height = '200px'
   flashSale.style.paddingBottom = '20px'
-  while (result.hasChildNodes()) {
-    result.removeChild(result.firstChild);
-  }
-  while (idFilterBrand.hasChildNodes()) {
-    idFilterBrand.removeChild(idFilterBrand.firstChild);
-  }
-  while (product.hasChildNodes()) {
-    product.removeChild(product.firstChild);
-  }
+  cleanMainDiv();
+  cleanCheckBbox();
 
   for (let i = 0; i < data.length; i++) {
     if (id === data[i].idProduct.toString()) {
@@ -461,12 +612,15 @@ function showProduct(id) {
 //  add event-Listeners
 refresh.addEventListener("click", refreshPage);
 wrapper.addEventListener("click", showByCategory);
-searchBtn.addEventListener("click", searchProduct);
+
 price.addEventListener("click", searchByPrice);
 storageDiv.addEventListener("click", searchByStorageType);
 displayTypeDiv.addEventListener("click", searchByDisplayType);
 idFilterBrand.addEventListener("click", searchByBrand);
-year.addEventListener("click", filterByYear);
+yearDiv.addEventListener("click", filterByYear);
+screenResolutionDiv.addEventListener("click", searchByScrResolution);
+colorsDiv.addEventListener("click", searchByColor);
+screenSizeDiv.addEventListener("click", searchByScrSize);
 
 
 result.addEventListener("click", function (event) {
@@ -475,6 +629,7 @@ result.addEventListener("click", function (event) {
   showProduct(item.id);
 });
 
+searchBtn.addEventListener("click", searchProduct);
 searchInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     searchProduct();
